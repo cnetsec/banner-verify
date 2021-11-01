96158715b4d58d4bb979b0cc81b4610640ace9e9
cursosnetsec <86935257+cursosnetsec@users.noreply.github.com>
2021-11-01 00:20:59 -0300

    Update banner.sh
    X Powered By

--- banner.sh
+++ banner.sh
@@ -11,22 +11,38 @@ do
 echo "[RESULTADOS $line]"
 WEBSITEHTTP=$(echo "http://$line")
 WEBSITEHTTPS=$(echo "https://$line")
-RESULTSERVERHTTP=$(echo | curl -s -I $WEBSITEHTTP --connect-timeout 2 | grep -e "Server: ")
-RESULTSERVERHTTPS=$(echo | curl -s -I $WEBSITEHTTPS --connect-timeout 2| grep -e "Server: ")
+RESULTHTTP_SERVER=$(echo | curl -s -I $WEBSITEHTTP --connect-timeout 2 | grep -e "Server: ")
+RESULTHTTPS_SERVER=$(echo | curl -s -I $WEBSITEHTTPS --connect-timeout 2| grep -e "Server: ")
+RESULTSHTTP_XPOWEREDBY=$(echo | curl -s -I $WEBSITEHTTP --connect-timeout 2 | grep -e "X-Powered-By: ")
+RESULTHTTPS_XPOWEREDBY=$(echo | curl -s -I $WEBSITEHTTPS --connect-timeout 2| grep -e "X-Powered-By: ")
 #Resultados apresentados em tela e salvos em uma lista BANNER-LIST.txt
-if [ -z "$RESULTSERVERHTTP" ];
+if [ -z "$RESULTHTTP_SERVER" ];
 then
         false
 else
-        echo " Resultado de acesso em Http  $RESULTSERVERHTTP "
-        echo $WEBSITEHTTP "-" $RESULTSERVERHTTP >> BANNER-LIST.txt
+        echo " Resultado de acesso em Http  $RESULTHTTP_SERVER "
+        echo $WEBSITEHTTP "-" $RESULTHTTP_SERVER >> BANNER-LIST.txt
 fi
-if [ -z "$RESULTSERVERHTTPS" ];
+if [ -z "$RESULTHTTPS_SERVER" ];
 then
         false
 else
-        echo " Resultado de acesso em Https $RESULTSERVERHTTPS"
-        echo $WEBSITEHTTPS "-" $RESULTSERVERHTTP >> BANNER-LIST.txt
+        echo " Resultado de acesso em Https $RESULTHTTPS_SERVER"
+        echo $WEBSITEHTTPS "-" $RESULTHTTPS_SERVER >> BANNER-LIST.txt
+fi
+if [ -z "$RESULTSHTTP_XPOWEREDBY" ];
+then
+        false
+else
+        echo " Resultado de acesso em Http  $RESULTSHTTP_XPOWEREDBY "
+        echo $WEBSITEHTTP "-" $RESULTSHTTP_XPOWEREDBY >> BANNER-LIST.txt
+fi
+if [ -z "$RESULTHTTPS_XPOWEREDBY" ];
+then
+        false
+else
+        echo " Resultado de acesso em Https $RESULTHTTPS_XPOWEREDBY"
+        echo $WEBSITEHTTPS "-" RESULTHTTPS_XPOWEREDBY >> BANNER-LIST.txt
 fi
 echo ""
 done <"$input"
